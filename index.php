95259cb3f1fbbb6a73bb7e3f1b65f7d18ff9092b
ainurcoding <ainurridwan98@gmail.com>
2021-08-07 14:26:47 +0700

    menambahkan paragraph

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum quaerat temporibus velit totam aut, voluptatem alias at sit quae vitae vel nostrum officia distinctio ratione assumenda reprehenderit eligendi ea optio.</p>
 </body>
 </html>
